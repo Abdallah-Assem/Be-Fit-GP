94d030f1b1b989d8d7cc916ea833e8c830066cc5
Abdallah Assem <102911851+Abdallah-Assem@users.noreply.github.com>
2023-06-08 18:02:08 +0300

    Revert "Exercises DTO"
    This reverts commit e6687f9f8ef273f0f40df42be034890c4d690198.

--- BeFit_Website/BeFit_Website/DTO/Exercises.cs
+++ BeFit_Website/BeFit_Website/DTO/Exercises.cs
@@ -1,15 +1,12 @@
 ﻿namespace BeFit_Website.DTO
 {
-    public class Exercise
+    public class Exercises
     {
-        public Guid id { get; set; }
         public string bodyPart { get; set; } = string.Empty;
         public string equipment { get; set; } = string.Empty;
         public string gifUrl { get; set; } = string.Empty;
+        public int id { get; set; }
         public string name { get; set; } = string.Empty;
-        public string SetsPlan { get; set; } = string.Empty;
         public string target { get; set; } = string.Empty;
-        public string plan { get; set; } = string.Empty;
-        public bool IsActive { get; set; }
     }
 }
